dac228b32c6769a62479f77017ba1beb0bda3eee
birds3345 <31601136+birds3345@users.noreply.github.com>
2024-01-20 18:44:38 -0500

    fixed errors in read me and reformatted code

--- TextGlitcher.lua
+++ TextGlitcher.lua
@@ -1,5 +1,5 @@
-local TweenService = game:GetService('TweenService')
-local RunService = game:GetService('RunService')
+local TweenService = game:GetService 'TweenService'
+local RunService = game:GetService 'RunService'
 
 
 --[===[
@@ -20,10 +20,10 @@ export type GlitchAction = {
 	tween: TweenBase
 }
 
-local StringToTable = function(str: string)
+local StringToTable = function(str: string): {string}
 	local res = {}
 
-	for c in str:gmatch'.' do
+	for c in str:gmatch '.' do
 		table.insert(res, c)
 	end
 
@@ -36,6 +36,7 @@ local PossibleGlitchCharacters: {string}, createGlitch do
 		':',';','?','.','>','<','=','+','-','_',')',
 		'(','*','&','%','$','#','@','!'
 	}
+	
 	createGlitch = function(amount: number, customCharacters: {string}?): string
 		--amount -= 1
 		if amount <= 0 then return '' end
@@ -59,7 +60,7 @@ local PossibleGlitchCharacters: {string}, createGlitch do
 		return s
 	end
 
-	math.randomseed(tick() ^ 6)
+	math.randomseed(os.clock() ^ 6)
 end
 
 
@@ -67,18 +68,20 @@ end
 		STYLES
   ]===]
 local StylesFunction = {} do
-	StylesFunction.EasingStyle = function(self, Int: IntValue)
+	StylesFunction.EasingStyle = function(self, Int: IntValue): ()
 		local FullString = self.FullString
-		
-		
+
+
 		local SingleGlitch = self.Glitched or createGlitch(#FullString, self.GlitchCharacters)
 
 		while not self.Completed do
 			local ThisGlitch = self.SingleGlitch and SingleGlitch:sub(self.Reversed and 1 or Int.Value, self.Reversed and #FullString-Int.Value or #FullString) or createGlitch(#FullString-Int.Value, self.GlitchCharacters)
 			self.TextDisplay.Text = self.Reversed and ThisGlitch .. FullString:sub(#FullString-Int.Value+1, #FullString) or FullString:sub(1, Int.Value) .. ThisGlitch
-			
+
 			local Letter = FullString:sub(Int.Value, Int.Value)
+			
 			self.UnglitchedEvent:Fire(Letter)
+			
 			task.wait(self.UpdateRate)
 		end
 	end
@@ -90,9 +93,11 @@ local StylesFunction = {} do
 		while not self.Completed do
 			local Suffix = self.MaintainEndTextAfterGlitch and (Int.Value == #FullString and '' or Suffix) or Suffix
 			self.TextDisplay.Text = FullString:sub(1, Int.Value) .. Suffix
-			
+
 			local Letter = FullString:sub(Int.Value, Int.Value)
+			
 			self.UnglitchedEvent:Fire(Letter)
+			
 			task.wait(self.UpdateRate)
 		end
 	end
@@ -103,14 +108,20 @@ local StylesFunction = {} do
 		local LastIndex, LastGlitch = 1, nil
 		while not self.Completed do
 			local newGlitch = createGlitch(1, self.GlitchCharacters)
+			
 			if self.SingleGlitch and LastGlitch and Int.Value == LastIndex then
 				newGlitch = LastGlitch
-			end;LastGlitch = newGlitch;
+			end
+			
+			LastGlitch = newGlitch
+			
 			self.TextDisplay.Text = FullString:sub(1, math.abs(Int.Value-1)) .. newGlitch .. FullString:sub(Int.Value+1)
 			LastIndex = Int.Value
-			
+
 			local Letter = FullString:sub(Int.Value, Int.Value)
+			
 			self.UnglitchedEvent:Fire(Letter)
+			
 			task.wait(self.UpdateRate)
 		end
 
@@ -129,6 +140,7 @@ local StringAction = {} do
 		if type(FullString) ~= 'string' then
 			warn('Miss used type for first argument; Use a string instead.')
 		end
+		
 		local self = setmetatable({}, StringAction)
 
 		self.FullString = FullString
@@ -137,7 +149,8 @@ local StringAction = {} do
 		self.SingleGlitch = info.SingleGlitch or false
 		self.MaintainEndTextAfterGlitch = info.MaintainEndTextAfterGlitch
 		self.UpdateRate = info.UpdateRate
-		self.UnglitchedEvent = Instance.new("BindableEvent")
+		self.Completed = false
+		self.UnglitchedEvent = Instance.new 'BindableEvent'
 
 		self.Suffix = info.Suffix or ''
 
@@ -148,7 +161,7 @@ local StringAction = {} do
 		return self
 	end
 
-	function StringAction:GlitchText(style: string?)
+	function StringAction:GlitchText(style: string?): ()
 		if self.tween and self:GetState() == Enum.PlaybackState.Playing then return warn('Can not glitch text while it is already being glitched') end
 		self.Completed = false
 
@@ -169,32 +182,37 @@ local StringAction = {} do
 	end
 
 	function StringAction:IsCompleted(): boolean
-		return self.Completed or false
+		return self.Completed
 	end
 
-	function StringAction:GlitchOnce(customCharacters: {string}?)
+	function StringAction:GlitchOnce(customCharacters: {string}?): ()
 		self.Glitched = createGlitch(#self.FullString, customCharacters or self.GlitchCharacters)
+
 		self.TextDisplay.Text = self.Glitched
 	end
 
-	function StringAction:Cancel()
+	function StringAction:Cancel(): ()
 		if not self.tween then return warn('There is no tween connected') end
+
 		self.Completed = true
 		self.tween:Cancel()
 	end
 
-	function StringAction:Pause()
+	function StringAction:Pause(): ()
 		if not self.tween then return warn('There is no tween connected') end
+
 		self.tween:Pause()
 	end
 
-	function StringAction:Resume()
+	function StringAction:Resume(): ()
 		if not self.tween then return warn('There is no tween connected') end
+
 		self.tween:Play()
 	end
 
 	function StringAction:GetState(): Enum.PlaybackState
 		if not self.tween then return warn('There is no tween connected') end
+		
 		return self.tween.PlaybackState
 	end
 end
